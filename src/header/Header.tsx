5adacb79e678285656426570cacdb527c06f595d
shukrullahshirzad <shukrullah.shirzad.779@gmail.com>
2026-01-05 17:49:41 +0430

    add styles to the header and add mantine themes

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,27 +1,29 @@
-import { Avatar } from "@mantine/core";
-import { IconBinoculars, IconSettings, IconBell } from "@tabler/icons-react";
+import { Avatar, Indicator } from "@mantine/core";
+import { IconBriefcase, IconSettings, IconBell } from "@tabler/icons-react";
 import avatar  from "../images/icons/avatar.png";
 const Header = () => {
   return <header
     className="w-full bg-mine-shaft-950 text-white p-4 h-20 flex justify-between items-center"
     >
     <div className="flex gap-1 items-center">
-      <IconBinoculars className="h-10 w-10"/>
-      <span className="text-2xl">
-        <span className="text-orange-400 text-4xl">i</span>Jobs
+      <IconBriefcase className="h-10 w-10 hover:text-bright-sun-400"/>
+      <span className="text-bright-sun-400 text-3xl">
+        Jobs Hub
       </span>
     </div>
     <div className="flex  items-center justify-around gap-6">
-      <a href="">Find Jobs</a>
-      <a href="">Find Talent</a>
-      <a href="">Post Job</a>
-      <a href="">About Us</a>
+      <a href="" className="hover:text-bright-sun-400">Find Jobs</a>
+      <a href="" className="hover:text-bright-sun-400">Find Talent</a>
+      <a href="" className="hover:text-bright-sun-400">Post Job</a>
+      <a href="" className="hover:text-bright-sun-400">About Us</a>
     </div>
-    <div className="flex gap-1 items-center">
-      <IconBell />
+    <div className="flex gap-3 items-center ">
       <span>User</span>
-      <Avatar src={avatar}/>
-      <IconSettings />
+      <Avatar src={avatar} />
+      <Indicator color="pink" size={8} offset={6} processing>
+        <IconBell className="bg-mine-shaft-900 p-1 rounded-full stroke-2 text-3xl hover:text-bright-sun-400"/>
+      </Indicator>
+      <IconSettings className="bg-mine-shaft-900 p-1 rounded-full hover:text-bright-sun-400"/>
     </div>
   </header>;
   
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,7 +3,7 @@ import Header from "../header/Header";
 
 const Homepage = () => {
   return (
-    <main className="min-h-screen bg-mine-shaft-950 text-white">
+    <main className="min-h-screen bg-mine-shaft-950 text-white font-['Poppins',sans-serif]">
       <Header />
     </main>
   ) 
